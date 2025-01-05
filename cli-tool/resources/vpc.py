8f23b2384ea7c86ddcb4b3f47d88c6306ec8accb
Sav <dsouvik141@gmail.com>
2025-01-05 23:51:14 +0530

    new: untested logic of using troposphere with cloudformation for creating VPC and Subnets is added

--- cli-tool/resources/vpc.py
+++ cli-tool/resources/vpc.py
@@ -4,6 +4,8 @@ from meta import DRY_RUN
 from uuid import uuid4
 import boto3
 from botocore.exceptions import ClientError
+from troposphere import Template, Parameter, Ref, GetAtt
+from troposphere.ec2 import VPC, Subnet
 from utils.logging import log, log_warning
 from services.config.env_config import EnvironmentConfig
 
@@ -16,25 +18,23 @@ class VPC:
         self.env_config = env_config
         self.config = env_config.config.vpc
         self.client = boto3.client('ec2', region_name=self.region)
+        self.template = Template()
+        self.template.set_description("CloudFormation template to manage VPC and Subnet")
 
     def _create(self):
         vpc_response = {"Vpc": None}
         try:
+            tags = self.config.get_tags()
+            tags.append({
+                'Key': 'Name',
+                'Value': self.config.vpc_name
+            })
             vpc_response = self.client.create_vpc(
                 CidrBlock=self.config.cidr,
                 TagSpecifications=[
                     {
                         'ResourceType': 'vpc',
-                        'Tags': [
-                            {
-                                'Key': 'Name',
-                                'Value': self.config.vpc_name
-                            },
-                            {
-                                'Key': 'Publisher',
-                                'Value': "kobidh"
-                            }
-                        ]
+                        'Tags': tags
                     },
                 ],
                 DryRun=DRY_RUN,
@@ -52,6 +52,11 @@ class VPC:
         for subnet in self.config.subnets["private"]:
             subnet_response = {"Subnet": None}
             try:
+                tags = self.config.get_tags()
+                tags.append({
+                    'Key': 'Name',
+                    'Value': subnet["name"]
+                })
                 subnet_response = self.client.create_subnet(
                     CidrBlock=str(subnet["cidr"]),
                     VpcId=self.config.vpc_id,
@@ -59,16 +64,7 @@ class VPC:
                     TagSpecifications=[
                         {
                             'ResourceType': 'subnet',
-                            'Tags': [
-                                {
-                                    'Key': 'Name',
-                                    'Value': subnet["name"]
-                                },
-                                {
-                                    'Key': 'Publisher',
-                                    'Value': "kobidh"
-                                }
-                            ]
+                            'Tags': tags
                         },
                     ],
                     DryRun=DRY_RUN
@@ -140,19 +136,21 @@ class VPC:
                 log_warning(
                     "DryRun passed: You have the required permissions to describe the Subnets.")
                 subnets_response = {"Subnets": [
-                    {"SubnetId": f"dummy-subnet-{"".join(str(uuid4()).split("-")[:3])}"}]}
+                    {"SubnetId": f"dummy-subnet-{"".join(str(uuid4()).split("-")[:3])}"}
+                ]}
         subnets_response['Vpc'] = vpc_response
         log(f"Subnets found with ids: {[resp.get("SubnetId") for resp in subnets_response.get("Subnets")]}")
         return subnets_response
 
     def _get_or_create(self):
         response = self._get()
-        if not response['Vpc']:
-            return self._create()
+        if not response.get('Vpc'):
+            response = self._create()
         self.config.vpc_id = response['Vpc']['VpcId']
         return response
 
     def _delete(self):
+        echo("Deleting VPC Resources...")
         response = self._get()
         if not response['Vpc']:
             return
@@ -185,7 +183,79 @@ class VPC:
         pass
 
     def run(self):
-        if self.env_config.get():
+        record = self.env_config.get()
+        if record and record.get('configuration', None) and record.get('configuration').get('vpc'):
             self._get_or_create()
         else:
             self._delete()
+
+
+    def _configure_vpc(self):
+        tags = self.config.get_tags()
+        tags.append({
+            'Key': 'Name',
+            'Value': self.config.vpc_name
+        })
+        vpc = VPC(
+            self.config.vpc_name,
+            CidrBlock=self.config.cidr,
+            EnableDnsSupport=False,
+            EnableDnsHostnames=False,
+            InstanceTenancy='default',
+            Tags=tags
+        )
+        self.template.add_resource(vpc)
+        private_subnets = []
+        for subnet in self.config.subnets["private"]:
+            tags = self.config.get_tags()
+            tags.append({
+                'Key': 'Name',
+                'Value': subnet["name"]
+            })
+            subnet = Subnet(
+                subnet["name"],
+                AvailabilityZone=subnet["az"],
+                CidrBlock=subnet["cidr"],
+                VpcId=Ref(vpc),
+                MapPublicIpOnLaunch=False,
+                Tags=tags
+            )
+            self.template.add_resource(subnet)
+            private_subnets.append(subnet)
+
+    def _deploy_template(self, stack_name):
+        client = boto3.client("cloudformation")
+        try:
+            # Check if stack exists
+            client.describe_stacks(StackName=stack_name)
+            echo(f"Stack {stack_name} already exists. Updating...")
+
+            # Update the stack
+            client.update_stack(
+                StackName=stack_name,
+                TemplateBody=self.template.to_json(),
+                Capabilities=["CAPABILITY_NAMED_IAM"],
+            )
+            echo(f"Stack {stack_name} updated successfully.")
+
+        except client.exceptions.ClientError as e:
+            if "does not exist" in str(e):
+                echo(f"Stack {stack_name} does not exist. Creating...")
+
+                # Create the stack
+                client.create_stack(
+                    StackName=stack_name,
+                    TemplateBody=self.template.to_json(),
+                    Capabilities=["CAPABILITY_NAMED_IAM"],
+                )
+                echo(f"Stack {stack_name} created successfully.")
+            else:
+                raise
+
+    def configure(self):
+        echo("Configuring VPC Resources...")
+        self._configure_vpc()
+
+    def apply(self):
+        self._deploy_template("TestVPCStack")
+
